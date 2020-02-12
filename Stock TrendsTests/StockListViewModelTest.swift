f3e4a9fb206dae0211b919febfcdb2ff0adbe582
Abilio Gambim Parada <abilio.gambimparada@altran.com>
2020-02-12 17:06:20 +0000

    [TDD] Add tests to StockListViewModel

---
+++ Stock TrendsTests/StockListViewModelTest.swift
@@ -0,0 +1,24 @@
+//
+//  StockListViewModelTest.swift
+//  Stock TrendsTests
+//
+//  Created by Abilio Gambim Parada on 12/02/2020.
+//  Copyright © 2020 Abilio Gambim Parada. All rights reserved.
+//
+
+import XCTest
+
+class StockListViewModelTest: XCTestCase {
+
+    func testShouldShowMostGainerWhenFetch() {
+
+    }
+
+    func testShouldShowMostLoserWhenFetch() {
+
+    }
+
+    func testShouldShowErrorWhenError() {
+
+    }
+}
