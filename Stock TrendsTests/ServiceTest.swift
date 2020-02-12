962b337fccd29769ad154fc6ddda89decd15265b
Abilio Gambim Parada <abilio.gambimparada@altran.com>
2020-02-12 14:45:18 +0000

    [TDD] Test for Services to fetch MostGainer and MostLoser stocks

---
+++ Stock TrendsTests/ServiceTest.swift
@@ -0,0 +1,22 @@
+//
+//  ServiceTest.swift
+//  Stock TrendsTests
+//
+//  Created by Abilio Gambim Parada on 12/02/2020.
+//  Copyright © 2020 Abilio Gambim Parada. All rights reserved.
+//
+
+import XCTest
+@testable import Stock_Trends
+
+class ServiceTest: XCTestCase {
+
+    func testShouldReturnMostGainer() {
+
+    }
+
+    func testShouldReturnMostLoser() {
+
+    }
+
+}
