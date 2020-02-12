6984428a400ffa3b86fe46978d4ee13f1f12bb1c
Abilio Gambim Parada <abilio.gambimparada@altran.com>
2020-02-12 11:57:40 +0000

    [Lint] Changes code to be under lint rules

--- Stock TrendsTests/StockTrendsTests.swift
+++ Stock TrendsTests/StockTrendsTests.swift
@@ -9,7 +9,7 @@
 import XCTest
 @testable import Stock_Trends
 
-class Stock_TrendsTests: XCTestCase {
+class StockTrendsTests: XCTestCase {
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
--- Stock TrendsUITests/StockTrendsUITests.swift
+++ Stock TrendsUITests/StockTrendsUITests.swift
@@ -8,7 +8,7 @@
 
 import XCTest
 
-class Stock_TrendsUITests: XCTestCase {
+class StockTrendsUITests: XCTestCase {
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
