b9d4a2d5f21d7fbe26d7a485507a9d3c512d6e27
Abilio Gambim Parada <abilio.gambimparada@altran.com>
2020-02-12 10:57:24 +0000

    Initial Commit

---
+++ Stock Trends/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Stock Trends
+//
+//  Created by Abilio Gambim Parada on 12/02/2020.
+//  Copyright © 2020 Abilio Gambim Parada. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
