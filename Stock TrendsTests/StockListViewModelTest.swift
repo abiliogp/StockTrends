c37c2dc0293c9375670c4de5188ded4b03427345
Abilio Gambim Parada <abilio.gambimparada@altran.com>
2020-02-13 15:32:31 +0000

    [TDD] Add test to check loading is shwon

--- Stock TrendsTests/StockListViewModelTest.swift
+++ Stock TrendsTests/StockListViewModelTest.swift
@@ -7,14 +7,35 @@
 //
 
 import XCTest
+import Combine
 @testable import Stock_Trends
 
 class StockListViewModelTest: XCTestCase {
 
     var viewModel = StockListViewModel()
 
-    func testShouldShowMostGainerWhenFetch() {
+    func testShouldPresentLoading() {
+        let expect = XCTestExpectation()
+        expect.expectedFulfillmentCount = 2
+
+        var loadingsSignal = [Bool]()
+
+        let sink = Subscribers.Sink<Bool, Never>(
+            receiveCompletion: { _ in },
+            receiveValue: {
+                loadingsSignal.append($0)
+                expect.fulfill()
+        })
+
+        viewModel.$loading.subscribe(sink)
+
         viewModel.setupController()
+
+        wait(for: [expect], timeout: 1.0)
+
+        XCTAssertFalse(loadingsSignal.first!)
+        XCTAssertTrue(loadingsSignal.last!)
+        XCTAssertEqual(loadingsSignal.count, 2)
     }
 
     func testShouldShowMostLoserWhenFetch() {
